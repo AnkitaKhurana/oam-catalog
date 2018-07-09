f16382558f3145d58ca71e870c3f7da8aad85c0c
sharkinsspatial <sharkinsgis@gmail.com>
2018-07-09 16:44:48 -0300

    Force cli exit when running script.

--- services/createAdmin.js
+++ services/createAdmin.js
@@ -1,18 +1,31 @@
+#!/bin/env node
+
+require('dotenv').config();
+var Conn = require('../services/db');
 const Admin = require('../models/admin');
+
 const bcrypt = require('bcrypt');
 
-const args = process.argv.slice(2);
-const name = args[0];
-const email = args[1];
-const password = args[2];
+var dbWrapper = new Conn();
 
-const hash = bcrypt.hashSync(password, 10);
-Admin.create({
-  name: name,
-  email: email,
-  password: hash
-}).then(admin => {
-  console.log('New Made:', admin);
-}).catch(function (err) {
-  console.log(err);
-});
+dbWrapper.start(createAdmin);
+
+function createAdmin () {
+  const args = process.argv.slice(2);
+  const name = args[0];
+  const email = args[1];
+  const password = args[2];
+  const hash = bcrypt.hashSync(password, 10);
+
+  Admin.create({
+    name: name,
+    email: email,
+    password: hash
+  }).then((admin) => {
+    console.log('New Made:', admin);
+    dbWrapper.close();
+  }).catch(function (err) {
+    console.error(err);
+    dbWrapper.close();
+  });
+}
